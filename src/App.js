30ec1edb1121591186ffc0b49378d9d2e250e569
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-22 14:39:59 +0530

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import { app } from "./firebase.config";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+import EncryptedMessage from "./components/EncryptedMessage";
+import NotFound from "./components/NotFound";
 
 function App() {
   console.log(app);
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/message/:id" element={<EncryptedMessage />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/EncryptedMessage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EncryptedMessage() {
+  return <div>EncryptedMessage</div>;
+}
+
+export default EncryptedMessage;
