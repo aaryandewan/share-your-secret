971f45af7d47cf5205570c3e949ebf1567ec33d7
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-22 19:33:57 +0530

    Homepage CSS is looking LOVE!

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,9 +3,25 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
 import { createEncryptedMessage } from "../services/firebase";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { Link } from "react-router-dom";
+
+const theme = createTheme();
+
+// theme.typography.h2 = {
+//   fontSize: "1.2rem",
+//   "@media (min-width:600px)": {
+//     fontSize: "1.5rem",
+//     fontFamily: "Raleway",
+//   },
+//   [theme.breakpoints.up("md")]: {
+//     fontSize: "3rem",
+//   },
+// };
 
 function Dashboard() {
   const [value, setValue] = useState("");
@@ -17,7 +33,9 @@ function Dashboard() {
 
     try {
       let docRefId = await createEncryptedMessage(value, "");
-      setEncryptedURL("/messages/" + docRefId.toString() + "/");
+      setEncryptedURL(
+        "http://localhost:3000/messages/" + docRefId.toString() + "/"
+      );
     } catch (e) {
       console.error("zyzz err", e);
     }
@@ -30,24 +48,79 @@ function Dashboard() {
 
   return (
     <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "50ch" },
-      }}
-      noValidate
-      autoComplete="off"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+      style={{ height: "100vh", backgroundColor: "" }}
     >
-      {<Link to={encryptedURL}>{encryptedURL}</Link>}
-      <TextField
-        id="outlined-multiline-static"
-        label="Add your secret message here..."
-        multiline
-        rows={4}
-        onChange={(e) => setValue(e.target.value)}
-      />
-      <Button onClick={handleSubmit} variant="outlined">
-        Submit
-      </Button>
+      <ThemeProvider theme={theme}>
+        <Grid
+          container
+          spacing={0}
+          direction="row"
+          justifyContent="center"
+          // alignItems="center"
+          alignItems="center"
+          style={{ backgroundColor: "", height: "50vh" }}
+        >
+          <Grid item xs={12} md={6} style={{ backgroundColor: "" }}>
+            <Typography
+              gutterBottom
+              textAlign="center"
+              sx={{ typography: { sm: "h2", xs: "h4" } }}
+            >
+              Share your secret below!
+            </Typography>
+          </Grid>
+
+          <Grid item md={7} xs={9} style={{ backgroundColor: "" }}>
+            <TextField
+              fullWidth
+              id="outlined-multiline-static"
+              label="Add your secret message here..."
+              multiline
+              rows={4}
+              onChange={(e) => setValue(e.target.value)}
+              // style={{ backgroundColor: "pink" }}
+            />
+          </Grid>
+          <Grid
+            item
+            md={6}
+            justifyContent="center"
+            style={{
+              backgroundColor: "",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Button onClick={handleSubmit} variant="outlined">
+              Submit your secret message!
+            </Button>
+          </Grid>
+          {encryptedURL && (
+            <Grid
+              item
+              xs={12}
+              justifyContent="center"
+              style={{
+                backgroundColor: "",
+                display: "flex",
+                flexDirection: "row",
+                justifyContet: "center",
+              }}
+            >
+              <div>
+                {/* <Typography variant="h6"> Your link is availabe at: </Typography> */}
+                <Typography sx={{ typography: { sm: "h6", xs: "body2" } }}>
+                  Your link is availabe at: {encryptedURL}
+                </Typography>
+              </div>
+            </Grid>
+          )}
+        </Grid>
+      </ThemeProvider>
     </Box>
   );
 }
